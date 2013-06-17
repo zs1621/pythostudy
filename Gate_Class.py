b5e413d11df7bcf2154b1593bced94c365e89434
zs1261 <zs1213yh@gmail.com>
2013-06-18 01:08:36 +0800

    add class define study-python-1

---
+++ Gate_Class.py
@@ -0,0 +1,24 @@
+class LogicGate:
+	def __init__(self, n):
+		self.label = n
+		self.output = None
+
+	def getLable(self):
+		return self.label
+
+	def getOutput(self):
+		self.output = self.performGateLogic()
+		return self.output
+
+class BinaryGate(LogicGate):
+	def __init__(self, n):
+		LogicGate.__init__(self, n)
+		self.pinA = None
+		self.pinB = None
+
+	def getPinA(self):
+		return int(input("Enter Pin A input for gate " + \
+				self.getLabel() + "-->"))
+	def getPinB(self):
+		return int(input("Enter Pin B input for gate " + \
+				self.getLabel() + "-->"))
