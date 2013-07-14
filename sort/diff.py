7b3955b64b67cdc5fb1f69f12f8b90958a9af84d
zs1621 <zs1213yh@gmail.com>
2013-07-14 23:43:50 +0800

    add result.json

--- sort/diff.py
+++ sort/diff.py
@@ -1,4 +1,5 @@
 import random
+import json
 import merge_sort
 import maopao
 import time
@@ -6,23 +7,32 @@ import radix_sort
 A = []
 B = [] 
 C = []
-ran = 9000000000000000000000000000000000000000000000000000000000000000000
+jon = []
+#ran = 9000000000000000000000000000000000000000000000000000000000000000000
 ran = 2 ** 140
-length = 20000
-for i in range(0,length):
-	num = random.randint(100000,ran)
-	A.append(num)
-	#B.append(num)
-	C.append(num)
+length = [1000, 10000, 30000, 50000]
+for lent in length:
+	for i in range(0,lent):
+		num = random.randint(100000,ran)
+		A.append(num)
+		#B.append(num)
+		C.append(num)
 
-start2 = time.clock()
-radix_sort.radixsort(C)
-print ("tong use time: ", time.clock() - start2)
+	start2 = time.clock()
+	radix_sort.radixsort(C)
+	radix_time = time.clock() - start2
+	print ("tong use time: ", time.clock() - start2)
 
 
-start = time.clock()
-merge_sort.sort(A, 0, len(A))
-print ("merge sort use: ", time.clock() - start )
+	start = time.clock()
+	merge_sort.sort(A, 0, len(A))
+	merge_time = time.clock() - start
+	print ("merge sort use: ", time.clock() - start )
+	jon.append({"x": lent, "mst": merge_time, "rst": radix_time})
+
+f = open('result.json','w')
+
+f.write(json.dumps(jon))
 
 #start1 = time.clock()
 #maopao.mp(B)
