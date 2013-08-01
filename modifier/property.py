a387863f41123691481581d4a342b10593a9c68c
zs1621 <zs1213yh@gmail.com>
2013-08-02 00:20:10 +0800

    fix property.py

--- modifier/property.py
+++ modifier/property.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
-#注意这里的Person() 括弧里的object ,有这个object的话  没有@aa.setter 那么p.aa 会报错
+#注意这里的Person() 括弧里的object ,有这个object的话  没有@aa.setter 那么p.aa 会报错 , 但是在第二个例子中 即使没有定义set 也可以赋值
+# 两者的区别就是前者是 new-style class ,后者是 old-style class 
+# 这是因为在new-style class 中， 规定是这样的
 class Person(object):
 	def __init__(self):
 		self.aaa = 6
