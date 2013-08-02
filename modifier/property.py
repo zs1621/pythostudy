9f33f2edbe3673f52e1c9f2848e61558f632f1db
zs1621 <zs1213yh@gmail.com>
2013-08-02 13:25:20 +0800

    fix property

--- modifier/property.py
+++ modifier/property.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-
+# 参考 [http://joy2everyone.iteye.com/blog/910950]
 #注意这里的Person() 括弧里的object ,有这个object的话  没有@aa.setter 那么p.aa 会报错 , 但是在第二个例子中 即使没有定义set 也可以赋值
 # 两者的区别就是前者是 new-style class ,后者是 old-style class 
 # 这是因为在new-style class 中， 规定是这样的
