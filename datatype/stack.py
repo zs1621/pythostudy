2ca81af2d108e2ad66e35bcfa9e9541ef00683a0
zs1621 <zs1213yh@gmail.com>
2013-08-14 14:31:20 +0800

    add two stack example

--- datatype/stack.py
+++ datatype/stack.py
@@ -62,3 +62,75 @@ def matches(open,close):
 
 
 print(parChecker('({[[}('))
+
+"""
+利用堆栈进行表达式转换  
+infix -> Postfix 
+"A + B * C" -> ABC*+
+"""
+def infixToPostfix(infixexpr):
+		prec = {}
+		prec["*"] = 3
+		prec["/"] = 3
+		prec["+"] = 2
+		prec["-"] = 2
+		prec["("] = 1
+		opStack = Stack()
+		postfixList = []
+		tokenList = infixexpr.split()
+
+		for token in tokenList:
+			if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+				postfixList.append(token)
+			#左括弧 推入栈 
+			elif token == '(':
+				opStack.push(token)
+			#如果遇到右括弧  
+			elif token == ')':
+				topToken = opStack.pop()
+				while topToken != '(':
+					postfixList.append(topToken)
+					topToken = opStack.pop()
+			else:
+				while (not opStack.isEmpty()) and \
+						(prec[opStack.peek()] >= prec[token] ):
+							postfixList.append(opStack.pop())
+				opStack.push(token)
+		while not  opStack.isEmpty():
+			postfixList.append(opStack.pop())
+		return "".join(postfixList)
+
+print (infixToPostfix("A + C * D"))
+
+
+"""
+Posfix evlauation
+计算  
+
+"""
+def postfixEval(postfixExpr):
+	operandStack = Stack()
+	tokenList = postfixExpr.split()
+
+	for token in tokenList:
+		if token in "0123456789":
+			operandStack.push(int(token))
+		else:
+			operand2 = operandStack.pop()
+			operand1 = operandStack.pop()
+			result = doMath(token, operand1, operand2)
+			operandStack.push(result)
+	return operandStack.pop()
+def doMath(op, op1, op2):
+	if op == "*":
+		return op1 * op2
+	elif op == "/":
+		return op1 / op2
+	elif op == "+":
+		return op1 + op2
+	else:
+		return op1 - op2
+
+
+
+print(postfixEval("7 8 *"))
