42571f7cacd53e49d840fe680e04d4a0e6ebe6d1
zs1621 <zs1213yh@gmail.com>
2013-08-21 22:15:02 +0800

    add recursive

---
+++ recursion/recursive.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+def listsum(numList):
+	if len(numList) == 1:
+		return numList[0]
+	else:
+		print numList[1:]
+		return numList[0] + listsum(numList[1:])
+	
+
+print (listsum([1,3,5,7,9]))
+
+
+def toStr(n, base):
+	convertString = "0123456789ABCDEF"
+	if n < base:
+		return convertString[n]
+	else:
+		return toStr(n // base, base) + convertString[n % base]
+
+print(toStr(1456,10))
+
+
+def check_palin(lister):
+	if  len(lister) <= 1 :
+		print 'True'
+		return True 
+	else:
+		if lister[0] == lister[-1]:
+			lister.pop()
+			del lister[0]
+			check_palin(lister)
+		else:
+			print  'False'
+			return False 
+
+def remove_white(string):
+	a = []
+	for i in string:
+		if i != ' ':
+			a.append(i)
+	return a
+
+
+print check_palin(remove_white('y - akay'))
+
+
+def ko(strd):
+	if strd == 1:
+		return True
+	else:
+		return False
+
+print ko(1)
